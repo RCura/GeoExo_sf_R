bb7f1822e8320dd4aa4b7560eb2b229062efea50
Hugues pecout <hugues.pecout@cnrs.fr>
2024-02-11 16:49:02 +0100

    update

--- Exercice_sf.R
+++ Exercice_sf.R
@@ -4,42 +4,47 @@ library(sf)
 st_layers("data/GeoSenegal.gpkg")
 
 pays <- st_read(dsn = "data/GeoSenegal.gpkg", layer = "Pays_voisins")
-loc <-st_read(dsn = "data/GeoSenegal.gpkg", layer = "Localites")
+sen <-st_read(dsn = "data/GeoSenegal.gpkg", layer = "Senegal")
 reg <-st_read(dsn = "data/GeoSenegal.gpkg", layer = "Regions")
+dep <-st_read(dsn = "data/GeoSenegal.gpkg", layer = "Departements")
+loc <-st_read(dsn = "data/GeoSenegal.gpkg", layer = "Localites")
 USSEIN <-st_read(dsn = "data/GeoSenegal.gpkg", layer = "USSEIN")
-# dep <-st_read(dsn = "data/GeoSenegal.gpkg", layer = "Departements")
-# loc <-st_read(dsn = "data/GeoSenegal.gpkg", layer = "Localites")
-
-road <-st_read(dsn = "data/TR_SEGMENT_ROUTIER_L.shp")
+routes <-st_read(dsn = "data/GeoSenegal.gpkg", layer = "Routes")
 
 
-loc$tt_services <- rowSums(loc[,5:17,drop=TRUE])
+# Carte de localités
+
+
+# Séléctionner uniquement les localités localisées au Sénégal
+# loc_sen <- loc[loc$PAYS == "SN", ]
+# OU
+loc_sen <- st_filter(x = loc, 
+                     y = sen,
+                     .predicate = st_intersects)
+
+# Nombre total de services dans les localités
+loc_sen$SERV_TT <- rowSums(loc_sen[,5:17,drop=TRUE])
 
 
 
-loc[loc$TYPELOCAL == -1 , "TYPELOCAL"] <- NA
+# Découper le réseau routier en fonction des limites du Sénégal
+routes_sen <- st_intersection(x = routes, y = sen)
 
 
-kaoloack <- loc[loc$NOM %in% "Kaolack", ]
+# st_write(obj = ..., dsn = "data/GeoSenegal.gpkg", layer = "...")
 
 
+# library(mapview)
+# mapview(USSEIN)
 
-library(mapview)
-mapview(USSEIN)
-
-# Import des données statistiques
-pop <- read.csv("data/Population_2015_2024.csv")
-
-# Jointure fond de carte région - données statistique région
-reg <- merge(reg, pop, by.x="NAME_1", by.y="NAME")
 
 
 # Carte thématique 1 - Cercle proportionnel
 library(mapsf)
 
-# mf_export(x = sen,
-#           filename = "img/carte_1.png",
-#           width = 800)
+mf_export(x = sen,
+          filename = "img/carte_1.png",
+          width = 800)
 
 mf_theme(bg = "steelblue3", fg= "grey10")
 
@@ -49,145 +54,299 @@ mf_map(pays, add = TRUE)
 mf_shadow(reg, add = TRUE)
 mf_map(reg, col = "grey95", add=T)
 
-mf_map(road, col = "grey50", add=T)
+mf_map(routes_sen, 
+       col = "grey50",
+       lwd = 0.4,
+       add = TRUE)
 
-mf_map(x = loc, 
-       var = c("tt_services", "TYPELOCAL"),
+mf_map(x = loc_sen, 
+       var = c("SERV_TT", "TYPELOCAL"),
        type = "prop_typo",
        pal = "Reds", 
        rev = TRUE,
-       inches = 0.08,
+       inches = 0.06,
        leg_pos = NA)
 
-
-mf_annotation(x = kaoloack, 
-              txt = "Kaolack", 
+mf_annotation(x = USSEIN, 
+              txt = "USSEIN", 
               halo = TRUE, 
               bg = "grey85",
-              cex = 1.5)
+              cex = 1.1)
+
+text(x = 261744.7, 
+     y = 1766915, 
+     labels = "Océan\nAtlantique", 
+     col="#FFFFFF99", cex = 0.65)
+
+text(x = 456008.1, 
+     y = 1490739, 
+     labels = "Gambie", 
+     col="#00000099", cex = 0.6)
+
+text(x = 496293.2, 
+     y = 1364960, 
+     labels = "Guinée-Bissau", 
+     col="#00000099", cex = 0.6)
+
+text(x = 748298.6, 
+     y = 1355112, 
+     labels = "Guinée", 
+     col="#00000099", cex = 0.6)
+
+text(x = 875867.9, 
+     y = 1541766, 
+     labels = "Mali", 
+     col="#00000099", cex = 0.6)
+
+text(x = 683394.9, 
+     y = 1818838, 
+     labels = "Mauritanie", 
+     col="#00000099", cex = 0.6)
 
 mf_legend(type = "typo", 
           val = c("Chef-lieu de région", 	
-                                 "Chef-lieu de département",
-                                 "Chef-lieu d’arrondissement",
-                                 "Chef-lieu de communauté rurale ",
-                                 "Commune",
-                                 "Village important",
-                                 "Village",
-                                 "Commune d’arrondissement",
-                                 "Habitat isolé"), 
+                  "Chef-lieu de département",
+                  "Chef-lieu d’arrondissement",
+                  "Chef-lieu de communauté rurale ",
+                  "Commune",
+                  "Village important",
+                  "Village",
+                  "Commune d’arrondissement",
+                  "Habitat isolé"), 
           pal = mf_get_pal(n = 9, palette = c("Reds")),
-          title = "Types de localités")
+          title_cex = 0.7,
+          val_cex = 0.5,
+          size = 0.6,
+          frame = TRUE,
+          bg = "#FFFFFF99",
+          title = "Statut des localités")
 
 mf_legend(type = "prop", 
-          val = c(1,2,3,4,5,6,7,8,9,10), 
-          inches = 0.08,
-          title = "Nombre de services",
+          val = c(1,3,5,7,10), 
+          inches = 0.06,
+          title_cex = 0.7,
+          title = "Nombre de\nservices",
           horiz = TRUE,
+          frame = TRUE,
+          bg = "#FFFFFF99",
           pos = "right")
 
-mf_title("Répartition de la population au Sénégal, par régions en 2024", fg = "white")
-mf_credits("Auteurs : Hugues Pecout\nSources : GADM & ANSD (2024)", cex = 0.5)
+mf_title("Répartition des localités sénégalaises en 2024", fg = "white")
+mf_credits("Auteurs : Hugues Pecout\nSources : GADM & GeoSénégal, 2014", cex = 0.5)
 
-
-
-
-c("Chef-lieu de région", 	
-"Chef-lieu de département",
-"Chef-lieu d’arrondissement",
-"Chef-lieu de communauté rurale ",
-"Commune",
-"Village important",
-"Village",
-"Commune d’arrondissement",
-"Habitat isolé")
-
-
-
-################################################################################
-# Carte des prix de l’immobilier (4 points)
-################################################################################ 
-
+dev.off()
 
 
 
 
 ################################################################################ 
-# Prix de l’immobilier dans le voisinnage de la Mairie de Montreuil (4 points)
+# Nombre école dans le voisinage de USSEIN
 ################################################################################ 
 
+st_crs(USSEIN)
 
+# Calcul d'un buffer de 5 kilomètres
+buf_5km <- st_buffer(USSEIN, 50000)
 
-# Calcul d'un buffer de 500 mètre
-buf <- st_buffer(USSEIN, 50000)
 # intersection entre les appartement et le buffer
-inter <- st_intersection(loc, buf)
+inters_loc_buff <- st_intersection(loc, buf_5km)
 
-mf_map(loc)
-mf_map(buf, col=NA,  add=TRUE)
+nb_loc_ecole_50km_USSEIN <- sum(inters_loc_buff$SERV_ECOLE)
 
-value <- sum(inter$SERV_ECOLE)
-
-cat(paste0("Le prix de l'immobilier dans un voisinnage de 500 mètres ",
-           "autour de la mairie de Montreuil est de ", 
-           round(value, 0), 
-           " eécole"))
-
-
+cat(paste0("Le nombre de localités abritant (au moins) une école dans un rayon de 50 km autour de l'",
+           USSEIN$NAME, " est de ", nb_loc_ecole_50km_USSEIN))
 
 
 
 ################################################################################ 
-# Utilisation d’un maillage régulier (4 points)
+# Utilisation d’un maillage régulier 
 ################################################################################ 
 
 # Créer une grille régulière avec st_make_grid()
-grid <- st_make_grid(reg, cellsize = 50000, square = TRUE)
+grid <- st_make_grid(sen, cellsize = 15000, square = TRUE)
 # Transformer la grille en objet sf avec st_sf()
 grid <- st_sf(geometry = grid)
+
 # Ajouter un identifiant unique, voir chapitre 3.7.6
-# dans https://rcarto.github.io/geomatique_avec_r/
 grid$id_grid <- 1:nrow(grid)
-# Compter le nombre de transaction dans chaque carreau, voir chapitre 3.7.7 
-# dans https://rcarto.github.io/geomatique_avec_r/
-inter_grid_loc<- st_intersects(grid, loc, sparse = TRUE)
-grid$n_loc <- sapply(inter_grid_loc, length)
-# Calculez le prix median par carreau, voir chapitre 3.7.8
-# dans https://rcarto.github.io/geomatique_avec_r/
-# st_intersection(), aggregate(), merge()
-# loc_grid <- st_intersection(loc, grid)
-# loc_agg <- aggregate(x = list(prixmed = loc_grid$tt_services),
-#                      by = list(id_grid = loc_grid$id_grid), 
-#                      FUN = median)
-# grid <- merge(x = grid, y = loc_agg, by = "id_grid", all.x = TRUE)
-# Selectionner les carreaux ayant plus de 10 transactions, voir chapitre 3.5
-# dans https://rcarto.github.io/geomatique_avec_r/
+
+# Compter le nombre de transaction dans chaque carreau
+grid_loc<- st_intersects(grid, loc, sparse = TRUE)
+grid$n_loc <- sapply(grid_loc, FUN = length)
 
 # Découpage de la grille en fonction des communes (optionel)
-grid <- st_intersection(grid, st_union(reg))
+grid_sen <- st_intersection(grid, sen)
 
 
 # Justification de la discrétisation (statistiques, boxplot, histogramme, 
 # beeswarm...)
-hist(grid$n_loc)
+hist(grid_sen$n_loc)
 
 # Cartographie
-bks <- mf_get_breaks(grid$n_loc ,nbreaks = 6, breaks =  "msd")
+
+library(mapsf)
+mf_export(x = sen,
+          filename = "img/carte_2.png",
+          width = 800)
+
+mf_theme(bg = "steelblue3", fg= "grey10")
+
+mf_map(x = reg, col = NA, border = NA)
+mf_map(pays, add = TRUE)
+
+mf_shadow(reg, add = TRUE)
+mf_map(reg, col = "grey95", add=T)
 
 
-mf_map(grid, "n_loc", "choro",
-       add = T, border = NA,
-       leg_pos = NA,breaks = bks, pal = "Burg")
-mf_map(road, lwd = .2, col ="#b5b3b5", add=T )
-mf_map(loc, add=T, col = "white", pch = ".", cex = .01)
+mf_map(x = grid_sen, 
+       var = "n_loc", 
+       type = "choro",
+       add = T, 
+       border = NA,
+       leg_pos = "topright",
+       leg_title = "Nombre de\nlocalités",
+       leg_val_rnd = 0,
+       breaks = c(0,0.1,1,2,3,4,5, max(grid_sen$n_loc)), 
+       pal = "SunsetDark")
+
+mf_map(routes_sen, lwd = .3, col ="#b5b3b5", add=T )
+mf_map(loc_sen, add=T, col = "white", pch = ".", cex = .1)
 mf_map(reg, col = NA, border = "white", add = T)
-mf_legend(type = "choro", pos = "bottomright", val = bks, 
-          pal = "Burg", val_rnd = -2, frame = T)
-mf_title("Brouillon")
+
+
+mf_annotation(x = USSEIN, 
+              txt = "USSEIN", 
+              halo = TRUE, 
+              bg = "grey85",
+              cex = 1.1)
+
+text(x = 261744.7, 
+     y = 1766915, 
+     labels = "Océan\nAtlantique", 
+     col="#FFFFFF99", cex = 0.65)
+
+text(x = 456008.1, 
+     y = 1490739, 
+     labels = "Gambie", 
+     col="#00000099", cex = 0.6)
+
+text(x = 496293.2, 
+     y = 1364960, 
+     labels = "Guinée-Bissau", 
+     col="#00000099", cex = 0.6)
+
+text(x = 748298.6, 
+     y = 1355112, 
+     labels = "Guinée", 
+     col="#00000099", cex = 0.6)
+
+text(x = 875867.9, 
+     y = 1541766, 
+     labels = "Mali", 
+     col="#00000099", cex = 0.6)
+
+text(x = 683394.9, 
+     y = 1818838, 
+     labels = "Mauritanie", 
+     col="#00000099", cex = 0.6)
+
+mf_title("Nombre de localités sénégalaises dans un carroaye de 15km", fg = "white")
+mf_credits("Auteurs : Hugues Pecout\nSources : GADM & GeoSénégal, 2014", cex = 0.5)
+
+dev.off()
+
+
+
+#---------------------------------------------------------------------------------------
+
+# BONUS
+
+library(potential)
+
+# create_grid() is used to create a regular grid with the extent of an existing layer (x) and a specific resolution (res).
+y <- create_grid(x = sen, res = 10000)
+
+# create_matrix() is used to compute distances between objects.
+d <- create_matrix(x = loc_sen, y = y)
+
+
+# The potential() function computes potentials.
+y$pot <- potential(x = loc_sen, y = y, d = d,
+                   var = "SERV_POSTE", fun = "e",
+                   span = 20000, beta = 2)
+
+# It’s possible to express the potential relatively to its maximum in order to display more understandable values (Rich 1980).
+y$pot2 <- 100 * y$pot / max(y$pot)
 
 
 
 
+# It’s also possible to compute areas of equipotential with equipotential().
+bks <- mf_get_breaks(y$pot, breaks = "q6")
+iso <- equipotential(x = y, var = "pot", breaks = bks, mask = sen)
 
 
+
+
+mf_theme(bg = "steelblue3", fg= "grey10")
+
+mf_map(x = reg, col = NA, border = NA)
+mf_map(pays, add = TRUE)
+
+mf_shadow(reg, add = TRUE)
+mf_map(reg, col = "grey95", add=T)
+
+
+mf_map(x = iso, var = "min", type = "choro", 
+       breaks = bks, 
+       pal = hcl.colors(10, 'Teal'),
+       lwd = .2,
+       border = "#121725", 
+       leg_pos = "topright",
+       leg_val_rnd = 0,
+       leg_title = "Potential of\nservices", add = TRUE)
+
+mf_map(routes_sen, lwd = .3, col ="#b5b3b5", add=T )
+mf_map(loc_sen, add=T, col = "white", pch = ".", cex = .1)
+mf_map(reg, col = NA, border = "white", add = T)
+
+
+mf_annotation(x = USSEIN, 
+              txt = "USSEIN", 
+              halo = TRUE, 
+              bg = "grey85",
+              cex = 1.1)
+
+text(x = 261744.7, 
+     y = 1766915, 
+     labels = "Océan\nAtlantique", 
+     col="#FFFFFF99", cex = 0.65)
+
+text(x = 456008.1, 
+     y = 1490739, 
+     labels = "Gambie", 
+     col="#00000099", cex = 0.6)
+
+text(x = 496293.2, 
+     y = 1364960, 
+     labels = "Guinée-Bissau", 
+     col="#00000099", cex = 0.6)
+
+text(x = 748298.6, 
+     y = 1355112, 
+     labels = "Guinée", 
+     col="#00000099", cex = 0.6)
+
+text(x = 875867.9, 
+     y = 1541766, 
+     labels = "Mali", 
+     col="#00000099", cex = 0.6)
+
+text(x = 683394.9, 
+     y = 1818838, 
+     labels = "Mauritanie", 
+     col="#00000099", cex = 0.6)
+
+mf_title("Potentiel d'accès à des services", fg = "white")
+mf_credits("Auteurs : Hugues Pecout\nSources : GADM & GeoSénégal, 2014", cex = 0.5)
 
--- README.md
+++ README.md
@@ -34,80 +34,148 @@ Les fichier de données sont mis à disposition dans le répertoire **data**, qu
     - **Senegal** : Couche des frontières du Sénégal. Source : https://gadm.org/, 2014   
     - **Regions** : Couche des régions sénégalaises. Source : https://gadm.org/, 2014   
     - **Departements** : Couche des Departements sénégalais. Source : https://gadm.org/, 2014   
-    - **Localites** : Couche de points des localités sénagalaises. Source : Base de données géospatiales prioritaires du Sénégal. https://www.geosenegal.gouv.sn/, 2014.   
-
-</br>
-
-- **Un fichier CSV** (**Population_2015_2024.csv**) qui contient des données sur la population des différentes régions sénégalaises de 2015 à 2014.
-
-Ces données ont été téléchargées depuis le portail de données de l'Agence Nationale de la Statisique et de la Démographie (ANSD, https://senegal.opendataforafrica.org/data/, 2014)
-
+    - **Localites** : Couche de points des localités sénagalaises. Source : Base de données géospatiales prioritaires du Sénégal. https://www.geosenegal.gouv.sn/, 2014. 
+    - **USSEIN** : Localisation de l'Université du Sine Saloum El-hâdj ibrahima NIASS. Source : Google Maps, 2014. 
+    - **Routes** : Couche du réseau routier sénégalais. Source : Base de données géospatiales prioritaires du Sénégal. https://www.geosenegal.gouv.sn/, 2014. 
 
 </br>
 
 
 ## **EXERCICE**
 
-#### **En vous appuyant sur le manuel [Geomatique avec R](https://rcarto.github.io/geomatique_avec_r/) et [Cartographie avec R](https://rcarto.github.io/cartographie_avec_r/), effectuez les opérations suivantes dans le fichier Exercice_sf.R :**
+#### **En vous appuyant sur les manuels [Geomatique avec R](https://rcarto.github.io/geomatique_avec_r/) et [Cartographie avec R](https://rcarto.github.io/cartographie_avec_r/), effectuez les opérations suivantes dans le fichier Exercice_sf.R :**
 
 </br>
 
 ##### A. Import des données
 
-importez les couche géographique suivante depuis les fichier GeoPackage ....gpkg :
+Importez l'ensemble des couches géographiques contenues dans le fichier GeoPackage **GeoSenegal.gpkg**.
 
-- Pays, 
-- Regions, limite...
-- Localité : 
-- USSEIN :
+    st_layers("data/GeoSenegal.gpkg")
 
+    ... <- st_read(dsn = "data/GeoSenegal.gpkg", layer = "...")
 
-- 
+</br>
 
-
-##### B. Affichage des données
+##### B. Séléction et intersection spatiale
 
 
-les régions sénégalaises et les localités dans la fenêtre graphique
+B.1 Séléctionnez (par attribut ou par localisation) uniquement les localités du Sénégal.
+
+    # Solution 1 - par attribut
+    ... <- ...[...$PAYS == "...", ]
+    
+    # Solution 2 - par localisation
+    ... <- st_filter(x = ..., y = ..., .predicate = st_intersects)
+
+B.2 Calculez le nombre de services présent dans chaque localité. Assignez le résultat dans une nouvelle colonne de la couche géographique des localités sénégalaises.
+
+    ...$... <- rowSums(...[, 5:17, drop=TRUE])
+    
+
+B.3 Découpez le réseau routier en fonction des limites du Sénégal.
+
+    ... <- st_intersection(x = ..., y = ...)
 
 
+</br>
 
-##### C.  
 
+##### C. Carte thématique des localités
+
+Construisez une carte thématique représentant les localités sénagalaise par leur nombre de services qu'elles abritent (symboles proportionnels) et par leur statut ("TYPELOCAL") représenter en couleur dans les symbols proportionnels. 
 
 Exemple :
 
 ![](img/carte_1.png)
 
 </br>
+    
+Pour vous aider, voici les étiquettes des différentes modialités préciser dans les métadonnées :  
 
-##### 2. Une carte choroplèthe représentant la densité de population par km2, par région en 2024. 
+- 1 = Chef-lieu de région    
+- 2 = Chef-lieu de département   
+- 3 = Chef-lieu d’arrondissement   
+- 4 = Chef-lieu de communauté rurale   
+- 5 = Commune   
+- 6 = Village important   
+- 7 = Village  
+- 8 = Commune d’arrondissement   
+- 9 = Habitat isolé   
 
-Exemple :
+
+      val = c("Chef-lieu de région", 
+              "Chef-lieu de département", 
+              "Chef-lieu d’arrondissement",
+              "Chef-lieu de communauté rurale", 
+              "Commune", 
+              "Village important", 
+              "Village",
+              "Commune d’arrondissement", 
+              "Habitat isolé")
+
+
+
+</br>
+
+
+##### D. Nombre d'écoles dans un rayon de 50km ?
+
+Calculez le nombre de localité qui abrite au moins une école ("SERV_ECOLE" dans la couche géographique des localités) dans un rayon de 50km (distance euclidienne) autour de l'Université du Sine Saloum El-hâdj ibrahima NIASS (USSEIN)
+
+D.1. Calculez un buffer de 50 km autour d'USSEIN
+
+    ... <- st_buffer(USSEIN, ...)
+
+D.2. Séléctionnez les localités situées dans la zone tampon de 50km
+
+    inters_loc_buff <- st_intersection(..., ...)
+    
+    
+D.3 Combien de ces localités abrite au moins une école ?    
+    
+    ... <- sum(inters_loc_buff$...)
+    
+
+</br>
+
+
+##### E. Utilisation d’un maillage régulier
+
+E.1 Créez un maillage régulier de carreaux de 50km de côté sur l'ensemble du Sénégal
+
+    grid <- st_make_grid(..., cellsize = ..., square = ...).
+    
+    # Transformer la grille en objet sf avec st_sf()
+    grid <- st_sf(geometry = grid)
+    
+    # Ajouter un identifiant unique à chaque carreaux
+    grid$id<- 1:nrow(grid)
+
+
+E.2 Récuperez le carreau d'appartenance (id) de chaque localité.
+
+    grid_loc <- st_intersects(..., ..., sparse = TRUE)
+    
+
+E.3 Comptez le nombre de localités dans chacun des carreaux.
+
+    grid$... <- sapply(grid_loc, FUN = ...)
+
+
+F. Enregistrez la grille régulière vectorielle de 50km de coté et découper par les limites de Sénégal dans le fichier **GeoSenegal.gpkg**
+
+    st_write(obj = ..., dsn = "data/GeoSenegal.gpkg", layer = "...")
+
+
+G. Construisez une carte représentant le nombre de localité par carreau.
+
+Exemple : 
 
 ![](img/carte_2.png)
 
-Pour calculer la surface du polygone, utilisez la fonction `st_area()` du package `sf` 
+Quelle critique pouvez-vous faire de cette carte thématique ?
 
-    reg$surface <- st_area(x = reg) 
-    
-Pour convertir l'unité de mesure de la surface calculée, utilisez la fonction  `set_units()` du package `units`
-    
-    library(units)
-    reg$surface <- set_units(reg$surface, km^2)
-
-</br>
-
-
-##### 3. Une carte de sotck et de ratio représentant les nombre d'habitants par région en 2024 (symbole proportionnel) et le taux d'évolution de la population (en %) entre 2015 et 2024 (en aplat de couleur dans les symboles proportionnels). 
-
-Exemple :
-
-![](img/carte_3.png)
-
-</br>
-
-$Taux\ d'évolution\ de\ la\ population\ entre\ 2015\ et\ 2014=\dfrac{Pop2024-Pop2015}{Pop2015}$
-
+H. BONUS - Calcul de potentiel d"'accessibilité à des services
 
 
